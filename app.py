95044e9b7f7239d77a79d919e2f94f401bd0a41c
Runtogetherradcliffe <runtogetherradcliffe@gmail.com>
2025-07-04 06:27:49 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -93,65 +93,6 @@ email_msg = "\n\n".join([
     footer,
     signoff
 ])
-
-
-{tour_msg}
-{location_line}
-{gmaps_line}
-{time_line}
-
-{route_section}
-
-{extra_msg}
-
-{footer}
-
-{signoff}"""
-
-
-facebook_msg = "\n\n".join([
-    "📣 " + intro,
-    tour_msg,
-    location_line,
-    gmaps_line,
-    time_line,
-    route_section.replace("*", ""),
-    extra_msg.replace("**", ""),
-    footer,
-    "👍 " + signoff
-])
-
-
-{tour_msg}
-{location_line}
-{gmaps_line}
-{time_line}
-
-{route_section}
-
-{extra_msg}
-
-{footer}
-
-👍 {signoff}"""
-
-whatsapp_msg = f"""*RunTogether Radcliffe – This Thursday!*
-
-{tour_msg}
-{location_line}
-{gmaps_line}
-{time_line}
-
-{route_section}
-
-{extra_msg}
-
-{footer}
-
-{signoff}"""
-
-# Display
-st.subheader("📧 Email Message")
 st.text_area("Email", value=email_msg, height=500)
 
 st.subheader("📱 Facebook / Instagram Post")
