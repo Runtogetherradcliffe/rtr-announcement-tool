971668d9e8a2ea157ac1451c4412a0e89cbf00f6
Runtogetherradcliffe <runtogetherradcliffe@gmail.com>
2025-07-13 17:53:41 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from route_summary_geocoding import summarize_routes
 from strava_utils import fetch_strava_activities
